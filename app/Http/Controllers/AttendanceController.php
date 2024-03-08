28ed2b56136189f9221e5a4199c74298ea5398f5
famensah-dev <famensah181@gmail.com>
2024-03-08 06:45:15 +0000

    Second stable version

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -29,7 +29,7 @@ class AttendanceController extends Controller
     public function store(Request $request)
     {
         $attendance = Attendance::create($request->all());
-        dd($attendance);
+        return view('welcome');
     }
 
     /**
